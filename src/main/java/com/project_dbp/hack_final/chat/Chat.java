31b8e5463c56b9c5ca64e264f2ddfdab6ee9c04a
rafram96 <rafael.ramos@utec.edu.pe>
2024-11-30 16:00:02 -0500

    Rest

--- src/main/java/com/project_dbp/hack_final/chat/Chat.java
+++ src/main/java/com/project_dbp/hack_final/chat/Chat.java
@@ -1,4 +1,15 @@
 package com.project_dbp.hack_final.chat;
 
+import com.project_dbp.hack_final.usuario.Usuario;
+import jakarta.persistence.*;
+
 public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 }
--- src/main/java/com/project_dbp/hack_final/message/Message.java
+++ src/main/java/com/project_dbp/hack_final/message/Message.java
@@ -1,4 +1,21 @@
 package com.project_dbp.hack_final.message;
 
+import jakarta.persistence.GeneratedValue;
+import com.project_dbp.hack_final.chat.Chat;
+import jakarta.persistence.*;
+
+@Entity
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "chat_id", nullable = false)
+    private Chat chatID;
+
+    private String content;
+
+    private DateTimestamp date;
 }
