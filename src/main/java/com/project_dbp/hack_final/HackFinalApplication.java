7d647627b1c9cbfef37b5188075ed0e2e20d3b47
rafram96 <rafael.ramos@utec.edu.pe>
2024-11-30 15:42:07 -0500

    other prank

---
+++ src/main/java/com/project_dbp/hack_final/HackFinalApplication.java
@@ -0,0 +1,13 @@
+package com.project_dbp.hack_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project_dbp/hack_final/HackFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_dbp.hack_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
